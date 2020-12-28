38f7edf57eb2099b6c9bae8b0bd273d7260a250b
himea-saito <57056612+himea-saito@users.noreply.github.com>
2020-12-28 04:45:17 -0700

    v0.5 Beta Update
    This update includes a minimap button and readme file!

---
+++ ReadMe.txt
@@ -0,0 +1,15 @@
+## Vaulty Addon ReadMe##
+Author: Himea
+Version: 0.5 Beta
+Updated: 10-28-2020
+WoW Version: 9.0.2
+
+Vaulty will load the Great Vault UI that is normally hidden and requires you to walk to Oribos to see.
+
+To open the frame, you can click the minimap button or type "/vaulty", "/gv", or "/greatvault".
+
+Download from:
+https://www.curseforge.com/wow/addons/vaulty
+https://github.com/himea-saito/Vaulty
+
+Use the GitHub URL to submit issues.
--- vaulty.lua
+++ vaulty.lua
@@ -5,7 +5,34 @@ local _, Vaulty = ...
 --Create chat command
 SLASH_VAULTY1, SLASH_VAULTY2, SLASH_VAULTY3 = "/gv", "/greatvault", "/vaulty"
 
---GV Functions
 function SlashCmdList.VAULTY(msg)
     WeeklyRewardsFrame:Show()
+end
+
+-- Call Ace LibDBIcon Minimap library https://www.wowace.com/projects/libdbicon-1-0
+-- This section creates the minimap button using .. ^
+Vaulty = LibStub("AceAddon-3.0"):NewAddon(Vaulty, "Vaulty", "AceConsole-3.0")
+
+VaultyLDB = LibStub("LibDataBroker-1.1"):NewDataObject("Vaulty", {
+	type = "data source",
+    text = "Vaulty",
+    --icon = "Interface\\Icons\\INV_Chest_Cloth_17",
+	icon = "Interface\\Addons\\Vaulty\\Images\\vaulty.tga",
+    OnClick = function()
+        WeeklyRewardsFrame:Show()
+    end,
+	OnTooltipShow = function(tt)
+    tt:AddLine("Vaulty")
+    tt:AddLine(" ")
+    tt:AddLine("Opens your vault.")
+	tt:AddLine(" ")
+	tt:AddLine("You can also type /vaulty, /gv, or /greatvault")
+    end
+})
+
+LibDBIcon = LibStub("LibDBIcon-1.0")
+
+function Vaulty:OnInitialize()
+    self.db = LibStub("AceDB-3.0"):New("VaultyDB", {profile = {minimap = {hide = false,},},})
+    LibDBIcon:Register("Vaulty", VaultyLDB, self.db.profile.minimap)
 end
